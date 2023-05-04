07f8a3e116d549a378d973a182510a5caace63da
saadrafi <saadrafi81@gmail.com>
2023-05-05 03:48:31 +0600

    treanding data send successfully

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
-const recipes=require("./data/recipes.json")
-const chefs =require("./data/chef's.json")
+const recipes = require("./data/recipes.json");
+const chefs = require("./data/chef's.json");
 
 app.use(cors());
 
 app.get("/chefs", (req, res) => {
   res.json(chefs);
+});
+
+app.get("/trending", (req, res) => {
+  const treanding = recipes.filter((recipe) => recipe.is_treanding === true);
+  res.json(treanding);
 });
 
 app.listen(port, () => {
